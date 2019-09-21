7a2900c141eb65a394232eb6c6619c9226b6241f
Cynthia Lee <35241156+Cynthia-Lee@users.noreply.github.com>
2019-09-21 05:16:34 -0400

    Added generating map floor

---
+++ Spaceship Shooter/Spaceship Shooter/Game.gd
@@ -0,0 +1,25 @@
+extends Spatial
+
+var ground_size = Vector2(40,40)
+
+var next_chunk_coord = Vector3(0,0,0)
+onready var map = $World/Ground
+onready var player = $PlayerShip
+
+func _ready():
+	create_chunk(ground_size.x, ground_size.y)
+
+func _physics_process(delta):
+	var next_trigger = (next_chunk_coord.z * 10) - (ground_size.y * 10)
+	#var next_trigger = (next_chunk_coord.z) - (ground_size.y)
+	if player.translation.z > next_trigger:
+		create_chunk(ground_size.x, ground_size.y)
+		
+func create_chunk(chunk_width, chunk_length):
+	for x in chunk_width:
+		for z in chunk_length:
+				var a = (randi() % 2)
+				map.set_cell_item(x, 0, z + next_chunk_coord.z + 1, a)
+
+			
+	next_chunk_coord.z += chunk_length
